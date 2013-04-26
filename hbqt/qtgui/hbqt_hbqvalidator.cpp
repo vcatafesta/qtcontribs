5bc527e5ada89885af1931efcfd2800bfb5d82da
bedipritpal <bedipritpal@d9dd3404-ddb2-4ebf-8b6a-858f5af37adf>
2013-04-26 20:51:53 +0000

    2013-04-26 13:49 UTC-0800 Pritpal Bedi (bedipritpal at hotmail.com)(r216)   * hbqt/qtgui/hbqt_hbqvalidator.cpp     ! Fixed: codepage dependent GETs to present proper alphabets.        This is achieved by replacing QString::toAscii() call with QString::toUtf8().

--- hbqt/qtgui/hbqt_hbqvalidator.cpp
+++ hbqt/qtgui/hbqt_hbqvalidator.cpp
@@ -123,7 +123,7 @@ QValidator::State HBQValidator::validate( QString & input, int & pos ) const
 
    if( block && hb_vmRequestReenter() )
    {
-      PHB_ITEM p0 = hb_itemPutStrUTF8( NULL, input.toLatin1().data() );
+      PHB_ITEM p0 = hb_itemPutStrUTF8( NULL, input.toUtf8().data() );
       PHB_ITEM p1 = hb_itemPutNI( NULL, pos );
 
       PHB_ITEM ret = hb_itemNew( hb_vmEvalBlockV( block, 2, p0, p1 ) );
